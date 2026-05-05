e7ddd77b91b791eca004dc35533db0b7f1b0e25a
Sadi Enis Güçlüer <gucluersadienis@gmail.com>
2026-05-05 16:20:59 +0300

    feat: add MySQL conn ping endpoint

---
+++ Controllers/DatabaseController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OceanClean.Api.Data;
+
+namespace OceanClean.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class DatabaseController : ControllerBase
+{
+    private readonly MySqlConnectionFactory _connectionFactory;
+
+    public DatabaseController(MySqlConnectionFactory connectionFactory)
+    {
+        _connectionFactory = connectionFactory;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Ping()
+    {
+        await using var connection = _connectionFactory.CreateConnection();
+        await connection.OpenAsync();
+
+        await using var command = connection.CreateCommand();
+        command.CommandText = "SELECT 1;";
+        
+        var result = await command.ExecuteScalarAsync();
+        return Ok(new
+        {
+            connected = true,
+            result,
+            timestamp = DateTime.UtcNow
+        });
+    }
+}
---
+++ Data/MySqlConnectionFactory.cs
@@ -0,0 +1,19 @@
+﻿using MySqlConnector;
+
+namespace OceanClean.Api.Data;
+
+public class MySqlConnectionFactory
+{
+    private readonly string _connectionString;
+
+    public MySqlConnectionFactory(IConfiguration configuration)
+    {
+        _connectionString = configuration.GetConnectionString("DefaultConnection")
+                            ?? throw new InvalidOperationException("DefaultConnection connection string is missing.");
+    }
+
+    public MySqlConnection CreateConnection()
+    {
+        return new MySqlConnection(_connectionString);
+    }
+}
